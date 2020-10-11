ff36802ca6e7e3b2f92f723b5c6a61a3f55303c2
christopher-henderson <chris@chenderson.org>
2020-10-11 11:02:07 -0700

    more versions

--- pstream/__init__.py
+++ pstream/__init__.py
@@ -27,5 +27,5 @@ from .stream import *
 import sys
 
 version = sys.version_info
-if version.major == 3 and version.minor >= 6:
+if version.major == 3 and version.minor >= 4:
     from .async_stream import *
