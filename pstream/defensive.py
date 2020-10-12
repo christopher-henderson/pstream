141da9920a90a49f0bafbfdb957a607e7b9e4f83
christopher-henderson <chris@chenderson.org>
2020-10-11 17:43:18 -0700

    fix py2

--- pstream/defensive.py
+++ pstream/defensive.py
@@ -7,6 +7,6 @@ def must_be_callable(fn):
     @wraps(fn)
     def inner(self, f, *args):
         if not callable(f):
-            raise NotCallableError(fn.__qualname__, f)
+            raise NotCallableError(fn.__qualname__ if hasattr(fn, '__qualname__') else fn.__name__, f)
         return fn(self, f, *args)
     return inner
